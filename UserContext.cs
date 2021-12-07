ccac40d50a422cb34f440f4f516b7f476e35c660
Johnson Dubula <jdubula@iqbusiness.net>
2021-12-07 17:38:27 +0200

    App8

---
+++ UserContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSevenFunction
+{
+   public class UserContext : DbContext
+    {
+        public UserContext() { }
+        public UserContext(DbContextOptions<UserContext> options)
+         : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=ASSET-10422\\SQLEXPRESS;Initial Catalog=AppSevenFuction;Integrated Security=True");
+        }
+    }
+}
